0ee69a380542b1dd6da444b21602d6c02c9a3e97
isaachrr4 <80857536+isaachrr4@users.noreply.github.com>
2022-05-05 17:03:43 -0400

    java practice files

---
+++ java-time/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("I Love being a Software Engineer!");
+        System.out.println(" It is a great Career!");
+        System.out.println("Hello World");
+    }
+}
---
+++ java-time/MyName.java
@@ -0,0 +1,9 @@
+public class MyName {
+    
+    public static void main(String[] args) {
+
+        System.out.println("Hi!, My name is Isaac Harris!");
+        System.out.println("I Love being a Software Engineer!");
+        System.out.println(" It is a great Career!");
+    }
+}
