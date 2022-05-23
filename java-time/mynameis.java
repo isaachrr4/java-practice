a07121b9614ae56cca0246c57616517df4c865ff
isaac harris <isaachrr4@gmail.com>
2022-05-23 11:58:23 -0400

    rev java demo

---
+++ java-time/mynameis.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class mynameis {
+    public static void main(String[] args) {
+        System.out.println("Hi, my name is java");
+
+        try (Scanner consoleReader = new Scanner(System.in)) {
+            String userInput = consoleReader.nextLine();
+            System.out.println("You said: " + userInput);
+        }
+    } 
+    
+}
